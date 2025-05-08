cee6ea068eb061a629625a6ba5c080c81c79652b
Galaxy9321 <matumatu-@hotmail.com>
2025-05-08 19:12:47 -0300

    Create ejercicio 8.py

---
+++ 04 Estructuras Repetitivas/ejercicio 8.py
@@ -0,0 +1,23 @@
+#Ejercicio 8
+
+pos = 0
+neg = 0
+par = 0
+impar = 0
+
+for i in range(6):
+    num = int(input("Ingrese numero entero: "))
+    if num % 2 == 0:
+        par = par + 1
+    else:
+        impar = impar + 1
+    if num > 0:
+        pos = pos + 1
+    else:
+        neg = neg + 1
+    
+
+print("Pares: ", par)
+print("Impares: ", impar)
+print("Negativos: ", neg)
+print("Positivos: ", pos)
