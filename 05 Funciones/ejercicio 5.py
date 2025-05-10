6e80b31d6d305e9c80a66dfe681bf3a9b86202f4
Galaxy9321 <matumatu-@hotmail.com>
2025-05-10 00:58:27 -0300

    Create ejercicio 5.py

---
+++ 05 Funciones/ejercicio 5.py
@@ -0,0 +1,11 @@
+#Ejercicio 5
+
+#programa a analizar:
+numeros = [8,15,3,22,7]
+numeros.remove(max(numeros))
+print(numeros)
+
+#Explicacion con mis palabras:
+#En la linea 4 hay una lista con los valores 8,15,3,22,7
+#En la siguiente linea remueve el numero mayor de esa lista
+#En la ultima linea del programa imprime por pantalla la lista actualizada
