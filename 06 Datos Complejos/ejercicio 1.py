cacc74a7a2dd2fa898ab001648b4893ed2edb642
Galaxy9321 <matumatu-@hotmail.com>
2025-06-07 18:53:46 -0300

    Update and rename ejercicio 1 to ejercicio 1.py

---
+++ 06 Datos Complejos/ejercicio 1.py
@@ -0,0 +1,8 @@
+#Diccionario dado:
+precios_frutas = {"Banana":1200, "Anana":2500, "Melon":3000, "Uva":1450}
+#Agregar Frutas con sus respectivos precios:
+precios_frutas["Naranja"] = 1200
+precios_frutas["Manzana"] = 1500
+precios_frutas["Pera"] = 2300
+
+print(precios_frutas)
