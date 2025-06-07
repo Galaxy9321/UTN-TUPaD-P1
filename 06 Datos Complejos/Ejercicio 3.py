a95e162858ce21c924ca93814e662b331a3741e0
Galaxy9321 <matumatu-@hotmail.com>
2025-06-07 19:03:45 -0300

    Create Ejercicio 3.py

---
+++ 06 Datos Complejos/Ejercicio 3.py
@@ -0,0 +1,13 @@
+#Agregar Frutas con sus respectivos precios:
+precios_frutas["Naranja"] = 1200
+precios_frutas["Manzana"] = 1500
+precios_frutas["Pera"] = 2300
+
+#Actualizacion de precios:
+precios_frutas["Banana"] = 1330
+precios_frutas["Manzana"] = 1700
+precios_frutas["Melon"] = 2800
+
+#Lista sin precios:
+sin_precios = list(precios_frutas.keys())
+print(sin_precios)
