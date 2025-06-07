65416827eed051de4190c077bdd1ceba00630486
Galaxy9321 <matumatu-@hotmail.com>
2025-06-07 18:56:07 -0300

    Create Ejercicio 2.py

---
+++ 06 Datos Complejos/Ejercicio 2.py
@@ -0,0 +1,13 @@
+#Diccionario dado:
+precios_frutas = {"Banana":1200, "Anana":2500, "Melon":3000, "Uva":1450}
+#Agregar Frutas con sus respectivos precios:
+precios_frutas["Naranja"] = 1200
+precios_frutas["Manzana"] = 1500
+precios_frutas["Pera"] = 2300
+#Actualizacion de precios:
+precios_frutas["Banana"] = 1330
+precios_frutas["Manzana"] = 1700
+precios_frutas["Melon"] = 2800
+
+
+print(precios_frutas)
